410158d8e1fb13ce0e4c8c6ab40a255647538bba
osboxes <yash.govil@gmail.com>
2020-05-03 01:20:15 -0400

    minor changes to client tester

--- time_client.sh
+++ time_client.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
 # short shell script to time the toy quic client for evaluation
 
-# modify the line below to change the number of requests client send to server
-requests=10
+array=(1280 20 40 80 160 320 640 1280)
 
-# modify the line below to navigate to wherever you installed the chromium src code
-cd /home/osboxes/chromium/src
-time ./out/Debug/quic_client --host=10.0.1.3 --port=6121 --disable_certificate_verification https://www.example.org --num_requests=$requests > scratch_output_294712
+for requests in "${array[@]}"
+do
+    echo ""
+    echo "Running the client with requests = $requests"
+    echo ""
+
+    # modify the line below to navigate to wherever you installed the chromium src code
+    cd /home/osboxes/chromium/src
+    time ./out/Debug/quic_client --host=10.0.1.3 --port=6121 --disable_certificate_verification https://www.example.org --num_requests=$requests > scratch_output_294712
+
+done
