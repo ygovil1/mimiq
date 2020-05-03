e3651867bc7d7f9692f0b4fe5b55bcfba2912ad0
osboxes <yash.govil@gmail.com>
2020-05-02 21:06:29 -0400

    fix bug in server script

--- start_server.sh
+++ start_server.sh
@@ -2,4 +2,4 @@
 
 # modify the line below to navigate to wherever you installed the chromium src code
 cd /home/osboxes/chromium/src
-./out/Debug/quic_server --quic_response_cache_dir=/home/osboxes/repos/quic-spine/website/www.example.org --certificate_file=net/tools/quic/certs/out/leaf_cert.pem --key_file=net/tools/quic/certs/out/leaf_cert.pkcs8 &
+./out/Debug/quic_server --quic_response_cache_dir=/home/osboxes/repos/mimiq/website/www.example.org --certificate_file=net/tools/quic/certs/out/leaf_cert.pem --key_file=net/tools/quic/certs/out/leaf_cert.pkcs8 &
