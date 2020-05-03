3d60ee2b1d9f23d27fd855daf4c5614463a6dc5c
osboxes <yash.govil@gmail.com>
2020-05-02 21:15:07 -0400

    some more bugfixes

--- time_client.sh
+++ time_client.sh
@@ -5,4 +5,4 @@ requests=10
 
 # modify the line below to navigate to wherever you installed the chromium src code
 cd /home/osboxes/chromium/src
-./out/Debug/quic_client --host=10.0.1.3 --port=6121 --disable_certificate_verification https://www.example.org --num_requests=$requests
+time ./out/Debug/quic_client --host=10.0.1.3 --port=6121 --disable_certificate_verification https://www.example.org --num_requests=$requests > scratch_output_294712
